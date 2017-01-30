1a2e3e5b7e13da20ad3ceaff4e4d5a6c847e31cf
Antoine Beaupré <anarcat@debian.org>
2017-01-30 03:58:10 -0500

    finish first prototype - this actually works now

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ import (
 	"path"
 	"regexp"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/Strubbl/wallabago"
@@ -29,7 +30,14 @@ import (
 // https://github.com/wallabag/wallabag/issues/2800
 var configJSON = flag.String("config", "config.json", "file name of config JSON file")
 var outputDir = flag.String("output", ".", "output directory to save files into")
+var count = flag.Int("count", 10, "number of articles to fetch")
 
+// cargo-culted from:
+// http://stackoverflow.com/questions/18207772/how-to-wait-for-all-goroutines-to-finish-without-using-time-sleep
+// probably wrong.
+var wg sync.WaitGroup
+
+// this is necessary because < 2.2 don't have a EPUB API
 func login(baseUrl, username, password string) *http.Client {
 	jar, _ := cookiejar.New(nil)
 	client := &http.Client{
@@ -60,9 +68,67 @@ func login(baseUrl, username, password string) *http.Client {
 	return client
 }
 
-func listIds(base_url string, ids chan int) {
-	entriesURL := base_url + "/api/entries.json?archive=0"
+// lightweight version of a wallabag entry with just what we need
+type Entry struct {
+	id      int
+	changed time.Time
+}
+
+/*
+the data format we receive from the API
+{
+  "page": 1,
+  "limit": 30,
+  "pages": 3,
+  "total": 64,
+  "_links": {
+    "self": {
+      "href": "https://example.net/wallabag/api/entries?archive=0&sort=created&order=desc&tags=&since=0&page=1&perPage=30"
+    },
+    "first": {
+      "href": "https://example.net/wallabag/api/entries?archive=0&sort=created&order=desc&tags=&since=0&page=1&perPage=30"
+    },
+    "last": {
+      "href": "https://example.net/wallabag/api/entries?archive=0&sort=created&order=desc&tags=&since=0&page=3&perPage=30"
+    },
+    "next": {
+      "href": "https://example.net/wallabag/api/entries?archive=0&sort=created&order=desc&tags=&since=0&page=2&perPage=30"
+    }
+  },
+  "_embedded": {
+    "items": [
+      {
+        "is_archived": 0,
+        "is_starred": 0,
+        "user_name": "joe",
+        "user_email": "joe@example.com",
+        "user_id": 3,
+        "tags": [],
+        "id": 23152
+        "created_at": "2014-06-13T12:18:34-0400",
+        "updated_at": "2016-11-29T20:02:16-0500",
+        "annotations": [],
+        "mimetype": "text/html",
+        "reading_time": 5,
+        "domain_name": "arstechnica.com",
+        "_links": {
+          "self": {
+            "href": "/api/entries/1579"
+          }
+        }
+      }
+    ]
+  }
+}
+
+*/
+
+// list entries and parse resulting JSON, sending them to the given channel
+func listEntries(base_url string, entries chan Entry) {
+	entriesURL := base_url + "/api/entries.json?archive=0&sort=updated&order=desc&perPage=" + strconv.Itoa(*count)
+	log.Println("fetching entries from", entriesURL)
 	body := wallabago.GetBodyOfAPIURL(entriesURL)
+	log.Printf("done. parsing %d bytes of JSON", len(body))
 	var parsed map[string]interface{}
 	if err := json.Unmarshal(body, &parsed); err != nil {
 		log.Fatal("failed to parse JSON from API: ", err)
@@ -71,44 +137,63 @@ func listIds(base_url string, ids chan int) {
 	//log.Print("parsed: ", data)
 	for _, v := range data {
 		d := v.(map[string]interface{})
-		ids <- int(d["id"].(float64))
+		changed, err := time.Parse("2006-01-02T15:04:05-0700", d["updated_at"].(string))
+		if err != nil {
+			log.Printf("can't parse date %s: %s", d["updated_at"], err)
+			changed = time.Now()
+		}
+		entry := Entry{id: int(d["id"].(float64)), changed: changed}
+		log.Printf("found entry %d modified on %s", entry.id, entry.changed)
+		entries <- entry
 	}
+	close(entries)
 }
 
-func download(client *http.Client, base_url string, id int) {
+// download a given entry in the right place
+func download(client *http.Client, base_url string, entry Entry) {
 	// proper way will be through the API, but for now we hardcode this URL
 	// https://github.com/wallabag/wallabag/pull/2372
 	// only in 2.2: /api/entries/123/export.epub
-	epubUrl := base_url + "/export/" + strconv.Itoa(id) + ".epub"
+	defer wg.Done()
+	//log.Println("received entry", entry)
+	epubUrl := base_url + "/export/" + strconv.Itoa(entry.id) + ".epub"
 	epub := path.Base(epubUrl)
 	output := path.Join(*outputDir, epub)
+	info, err := os.Stat(output)
+	if err == nil && info.ModTime().After(entry.changed) && info.Size() > 0 {
+		log.Printf("URL %s older than local file %s, skipped", epubUrl, output)
+		return
+	} else if err != nil {
+		//log.Println("missing:", err)
+	} else {
+		//log.Printf("out of date: err: %s, modtime: %s, changed: %s, before? : %s", err, info.ModTime(), entry.changed, info.ModTime().Before(entry.changed))
+	}
 	log.Printf("downloading %s in %s", epubUrl, output)
 	out, err := os.Create(output)
+	if err != nil {
+		log.Fatal("failed to create output file: ", err)
+	}
 	defer out.Close()
 	//body = wallabago.GetBodyOfAPIURL(epubUrl)
 	//out.Write(body)
 	resp, err := client.Get(epubUrl)
-	log.Print(resp, err)
+	log.Println("received response:", resp, err)
 	defer resp.Body.Close()
 	n, err := io.Copy(out, resp.Body)
 	log.Printf("wrote %d bytes in file %s", n, output)
-	// this would be the proper way:
-	// _, err = io.Copy(out, resp.Body)
-	// but we don't get the response back from wallabago - instead it reads the whole body
 }
 
 func main() {
 	start := time.Now()
 	log.SetOutput(os.Stdout)
 	defer func() {
-		log.Printf("printElapsedTime: time elapsed %.2fs\n", time.Since(start).Seconds())
+		log.Printf("completed in %.2fs\n", time.Since(start).Seconds())
 	}()
 	flag.Parse()
 	config, err := getConfig()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	log.Println("main: setting wallabago.Config var")
 	wallabago.Config = config
 	/* entries := wallabago.GetEntries(0, -1, "", "", -1, -1, "")
 	log.Printf("found %d unread entries", entries.Total)
@@ -116,10 +201,14 @@ func main() {
 	/* for entry := range entries {
 		log.Print(entry)
 	} */
+	log.Println("logging in to", config.WallabagURL)
 	client := login(config.WallabagURL, config.UserName, config.UserPassword)
-	ids := make(chan int)
-	go listIds(config.WallabagURL, ids)
-	for id := range ids {
-		go download(client, config.WallabagURL, id)
+	entries := make(chan Entry)
+	go listEntries(config.WallabagURL, entries)
+	for entry := range entries {
+		//log.Println("dispatching", entry)
+		wg.Add(1)
+		go download(client, config.WallabagURL, entry)
 	}
+	wg.Wait()
 }
