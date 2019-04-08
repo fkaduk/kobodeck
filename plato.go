fed1e444981b1bb60dca306adb0a993c65327d17
Antoine Beaupré <anarcat@debian.org>
2019-04-07 21:56:28 -0400

    add plato metadata parsing code

---
+++ plato.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type platoMetadata struct {
+	title      interface{}
+	author     interface{}
+	year       interface{}
+	publisher  interface{}
+	categories interface{}
+	File       platoFileMetadata
+	Reader     platoMetadataReader `json:"reader,omitempty"`
+	added      interface{}
+}
+
+type platoFileMetadata struct {
+	Path string
+	kind interface{}
+	size interface{}
+}
+
+type platoMetadataReader struct {
+	opened      interface{}
+	currentPage interface{}
+	pagesCount  interface{}
+	Finished    bool `json:"finished"`
+	rotation    interface{}
+	fontSize    interface{}
+}
+
+func parsePlatoMetadata(path string) (meta []platoMetadata, err error) {
+	raw, err := ioutil.ReadFile(path)
+	if err != nil {
+		return meta, err
+	}
+	err = json.Unmarshal(raw, &meta)
+	return meta, err
+}
+
+func checkPlatoStatus(bookPath string) (res bool) {
+	for _, entry := range meta {
+		if entry.Reader.Finished && bookPath == entry.File.Path {
+			return true
+		}
+	}
+	return res
+}
+
+var (
+	parsed bool
+	meta   []platoMetadata
+)
+
+func readPlatoStatus(ID int) (res bool, err error) {
+	if !parsed {
+		meta, err = parsePlatoMetadata("/mnt/onboard/.metadata.json")
+	}
+	if err != nil {
+		return res, err
+	}
+	// XXX: similar code in readKoreaderStatus, getting messy and hardcode-y
+	path := fmt.Sprintf("wallabako/%d.epub", ID)
+	res = checkPlatoStatus(path)
+	return res, err
+}
---
+++ plato_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestReadPlatoStatus(t *testing.T) {
+	var err error
+	meta, err = parsePlatoMetadata("metadata-plato-sample.json")
+	if err != nil {
+		t.Errorf("failure to setup test suite: %v", err)
+	}
+	res := checkPlatoStatus("wallabako/32827.epub")
+	if !res {
+		t.Errorf("Book status was incorrect, got: %v, want: %v", res, true)
+	}
+}
