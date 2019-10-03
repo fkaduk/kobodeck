a7341a835ca2ac8a0d598ed13be32d7f056f0616
Antoine Beaupré <anarcat@debian.org>
2019-10-03 09:32:04 -0400

    clarify where plato test failures are, if any

--- plato_test.go
+++ plato_test.go
@@ -10,14 +10,14 @@ func TestReadPlatoStatus(t *testing.T) {
 	}
 	res := checkPlatoStatus("wallabako/32827.epub")
 	if res != bookRead {
-		t.Errorf("Book status was incorrect, got: %v, want: %v", res, bookRead)
+		t.Errorf("Book status was incorrect, got: %v, want: %v (read)", res, bookRead)
 	}
 	res = checkPlatoStatus("wallabako/35130.epub")
 	if res != bookReading {
-		t.Errorf("Book status was incorrect, got: %v, want: %v", res, bookReading)
+		t.Errorf("Book status was incorrect, got: %v, want: %v (reading)", res, bookReading)
 	}
 	res = checkPlatoStatus("wallabako/1.epub")
 	if res != bookUnread {
-		t.Errorf("Book status was incorrect, got: %v, want: %v", res, bookUnread)
+		t.Errorf("Book status was incorrect, got: %v, want: %v (unread)", res, bookUnread)
 	}
 }
