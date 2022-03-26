684020d311579626b43bd429e138662bd3ed62dc
Antoine Beaupré <anarcat@debian.org>
2022-03-25 20:49:26 -0400

    replace "kobo" references by "nickel" where appropriate
    We prefer to use "nickel" internally because a lot of things actually
    without "Nickel" (the default Kobo UI) on the Kobo. This is liberating
    and shows how that's just one another (and kind of buggy)
    implementation of a book reader UI that we don't necessarily have to
    deal with all the time...
    
    In fact, this makes me realize I should make wallabako more of
    device-agnostic program. For example, it could be used with koreader
    wherever *that* program is ported, which is a lot of devices, way more
    than just Kobos.

--- nickel.go
+++ nickel.go
@@ -1,3 +1,18 @@
+/*
+
+Parse read status from the Nickel UI database.
+
+Nickel is Kobo's builtin and proprietary UI which stores book details
+in a SQLite database.
+
+We refer to it as "Nickel" here because that's the internal name used
+by Kobo. This is to distinguish this UI from the Kobo *device* itself,
+which also happens to run other UIs and programs like koreader, Plato,
+etc.
+
+The code simply reads the book status from the sqlite database.
+
+*/
 package main
 
 import (
@@ -8,21 +23,21 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-// koboNormalBook is the ContentID code for normal books in the Kobo sqlite database
-const koboNormalBook = 6
+// nickelNormalBook is the ContentID code for normal books in the Nickel sqlite database
+const nickelNormalBook = 6
 
-// the book status stored in the Kobo database
-type koboBookStatus int
+// the book status stored in the Nickel database
+type nickelBookStatus int
 
-// those happen to be incremental identifiers in the Kobo database,
+// those happen to be incremental identifiers in the Nickel database,
 // starting at zero
 const (
-	koboBookUnread koboBookStatus = iota
-	koboBookReading
-	koboBookRead
+	nickelBookUnread nickelBookStatus = iota
+	nickelBookReading
+	nickelBookRead
 )
 
-func readKoboStatus(ID int, outputDir string) (res bookStatus, err error) {
+func readNickelStatus(ID int, outputDir string) (res bookStatus, err error) {
 	if len(config.Database) <= 0 {
 		return res, fmt.Errorf("no database configured")
 	}
@@ -35,15 +50,15 @@ func readKoboStatus(ID int, outputDir string) (res bookStatus, err error) {
 	defer db.Close()
 
 	path := fmt.Sprintf("file://%s/%d.epub", outputDir, ID)
-	rows, err := db.Query("SELECT ReadStatus FROM content WHERE ContentID = $1 AND ContentType = $2 LIMIT 1", path, koboNormalBook)
+	rows, err := db.Query("SELECT ReadStatus FROM content WHERE ContentID = $1 AND ContentType = $2 LIMIT 1", path, nickelNormalBook)
 	if err != nil {
 		return res, err
 	}
 	defer rows.Close()
-	var readStatus koboBookStatus
+	var readStatus nickelBookStatus
 	if rows.Next() {
 		if err = rows.Scan(&readStatus); err == nil {
-			debugln("found Kobo readStatus", readStatus)
+			debugln("found Nickel readStatus", readStatus)
 		} else {
 			debugln("error scanning readstatus", err)
 		}
@@ -51,13 +66,13 @@ func readKoboStatus(ID int, outputDir string) (res bookStatus, err error) {
 		err = rows.Err()
 	}
 	switch readStatus {
-	case koboBookUnread:
+	case nickelBookUnread:
 		return bookUnread, err
-	case koboBookReading:
+	case nickelBookReading:
 		return bookReading, err
-	case koboBookRead:
+	case nickelBookRead:
 		return bookRead, err
 	}
-	log.Printf("warning: unexpected Kobo book state: %d, assuming reading\n", readStatus)
+	log.Printf("warning: unexpected Nickel book state: %d, assuming reading\n", readStatus)
 	return bookReading, err
 }
