4c8fb9b48cba2a00564bb0b598d1ea5de2952fa0
Antoine Beaupré <anarcat@debian.org>
2023-06-09 20:48:09 -0400

    make cat logger work
    The trick was that commands don't inherit stdout/stderr in go, so
    it *was* working, I was just not seeing it.
    
    To be fair with ChatGPT, it *did* have that trick in there, just
    completely unexplained (and actually not relevant for my final use
    case, just coincidentally allowing me to confirm this works here).

--- cmds/cat.go
+++ cmds/cat.go
@@ -1,10 +1,14 @@
 package main
 
 import "log"
+import "os"
 import "os/exec"
 
 func main() {
+	log.Printf("starting")
 	cmd := exec.Command("cat")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	stdout, err := cmd.StdinPipe()
 
 	if err != nil {
@@ -16,9 +20,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cmd.Wait()
+	log.Printf("started cat")
 
 	log.SetOutput(stdout)
-	log.Printf("test logging to cat")
-	stdout.Close()
+	log.Printf("set output")
+	log.Printf("test logging to cat\n")
+	err = stdout.Close()
+	err2 := cmd.Wait()
+	log.SetOutput(os.Stdout)
+	log.Printf("Command finished with error: %v", err)
+	log.Printf("Command finished with error: %v", err2)
 }
