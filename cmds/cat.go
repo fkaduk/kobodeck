97121b0b603f3715c98436f2d89f12e47d937c54
Antoine Beaupré <anarcat@debian.org>
2023-06-09 23:04:08 -0400

    show how this doesn't work
    the data gets flushed to stdout only when the stream is closed on
    cat(1) because it's not line-based, it's stream-based.
    
    And this is all completely irrelevant anyway because fbink takes
    arguments from stdin, as far as my docs say.

--- cmds/cat.go
+++ cmds/cat.go
@@ -10,14 +10,15 @@ func main() {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	stdout, err := cmd.StdinPipe()
-
 	if err != nil {
+		log.Printf("stdinpipe failed")
 		log.Fatal(err)
 	}
 	defer stdout.Close()
 
 	err = cmd.Start()
 	if err != nil {
+		log.Printf("start failed")
 		log.Fatal(err)
 	}
 	log.Printf("started cat")
@@ -25,9 +26,11 @@ func main() {
 	log.SetOutput(stdout)
 	log.Printf("set output")
 	log.Printf("test logging to cat\n")
-	err = stdout.Close()
-	err2 := cmd.Wait()
 	log.SetOutput(os.Stdout)
+	log.Printf("before close")
+	err = stdout.Close()
+	log.Printf("before wait")
+	err2 := cmd.Wait()
 	log.Printf("Command finished with error: %v", err)
 	log.Printf("Command finished with error: %v", err2)
 }
