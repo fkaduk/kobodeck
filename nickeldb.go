09d2fdc7e0c23ea119e5431912e6ec3653e5c338
fkaduk <fkaduk@proton.me>
2026-05-03 22:30:18 +0200

    simplify nickelDB access to a single function

--- nickeldb.go
+++ nickeldb.go
@@ -16,65 +16,32 @@ const (
 	bookRead    bookStatus = 2
 )
 
-func readStatus(ID string, outputDir string) (bookStatus, error) {
-	if len(nickelDB) > 0 {
-		res, err := readNickelStatus(ID, outputDir)
-		debugf("nickel book %s status: %d", ID, res)
-		return res, err
-	}
-	return bookUnread, nil
-}
-
 const nickelNormalBook = 6
 
-type nickelBookStatus int
-
-const (
-	nickelBookUnread  nickelBookStatus = 0
-	nickelBookReading nickelBookStatus = 1
-	nickelBookRead    nickelBookStatus = 2
-)
-
-func readNickelStatus(ID string, outputDir string) (res bookStatus, err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			log.Println("failed to read Nickel status:", r)
-		}
-	}()
-	if len(nickelDB) <= 0 {
-		return res, fmt.Errorf("no database configured")
-	}
+func readStatus(ID string, outputDir string) (bookStatus, error) {
 	db, err := sql.Open("sqlite", nickelDB)
 	if err != nil {
-		return res, err
+		return bookUnread, err
 	}
 	defer db.Close()
 
 	path := fmt.Sprintf("file://%s/%s.epub", outputDir, ID)
-	rows, err := db.Query("SELECT ReadStatus FROM content WHERE ContentID = $1 AND ContentType = $2 LIMIT 1", path, nickelNormalBook)
-	if err != nil {
-		return res, err
-	}
-	defer rows.Close()
-
-	var readStatus nickelBookStatus
-	if rows.Next() {
-		if err = rows.Scan(&readStatus); err != nil {
-			debugln("error scanning readstatus", err)
-		} else {
-			debugln("found Nickel readStatus", readStatus)
-		}
-	} else {
-		err = rows.Err()
-	}
-	switch readStatus {
-	case nickelBookUnread:
+	row := db.QueryRow("SELECT ReadStatus FROM content WHERE ContentID = $1 AND ContentType = $2 LIMIT 1", path, nickelNormalBook)
+	var status int
+	if err := row.Scan(&status); err == sql.ErrNoRows {
+		return bookUnread, nil
+	} else if err != nil {
 		return bookUnread, err
-	case nickelBookReading:
-		return bookReading, err
-	case nickelBookRead:
-		return bookRead, err
 	}
-	log.Printf("warning: unexpected Nickel book state: %d, assuming reading\n", readStatus)
-	return bookReading, err
+	debugf("nickel book %s status: %d", ID, status)
+	switch bookStatus(status) {
+	case bookUnread:
+		return bookUnread, nil
+	case bookReading:
+		return bookReading, nil
+	case bookRead:
+		return bookRead, nil
+	}
+	log.Printf("warning: unexpected Nickel book state: %d, assuming reading", status)
+	return bookReading, nil
 }
