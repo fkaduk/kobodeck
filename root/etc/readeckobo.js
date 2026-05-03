e2997870eb0b10769821ebc07d8ad8e60356ca53
fkaduk <fkaduk@proton.me>
2026-05-03 12:23:44 +0200

    add all config params

--- root/etc/readeckobo.js
+++ root/etc/readeckobo.js
@@ -1,4 +1,14 @@
 {
   "ReadeckURL": "https://example.com/readeck",
-  "Token": ""
+  "Token": "",
+  "OutputDir": "/mnt/onboard/readeckobo",
+  "Database": "/mnt/onboard/.kobo/KoboReader.sqlite",
+  "Concurrency": 2,
+  "Count": -1,
+  "Timeout": 300,
+  "Delete": false,
+  "Tags": "",
+  "LogFile": "",
+  "PidFile": "",
+  "Debug": false
 }
