190a3eb7ae2026cd958e325e3ec5d9b6a7d3f970
fkaduk <fkaduk@proton.me>
2026-09-22 12:00:00 +0200

    WIP cleanup

--- cleanup.go
+++ cleanup.go
@@ -7,13 +7,14 @@ import (
 	"os"
 )
 
-// TODO: add descriptions to both
+// closeWithWarning closes a resource and logs a non-fatal close error.
 func closeWithWarning(name string, closer io.Closer) {
 	if err := closer.Close(); err != nil {
 		log.Printf("warning: close %s: %v", name, err)
 	}
 }
 
+// removeWithWarning removes a path and ignores the already-absent case.
 func removeWithWarning(path string) {
 	if err := os.Remove(path); err != nil && !errors.Is(err, os.ErrNotExist) {
 		log.Printf("warning: remove %s: %v", path, err)
